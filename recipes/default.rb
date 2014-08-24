46ce1043052a7059ba35716dbb2dd2eb49116073
Kenji Akiyama <artifactsauce@gmail.com>
2014-08-24 15:38:59 +0900

    install btsync pacakge.

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,16 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+include_recipe "apt"
+
+apt_repository 'btsync' do
+  uri 'http://ppa.launchpad.net/tuxpoldo/btsync/ubuntu'
+  distribution 'trusty'
+  components   ['main']
+  keyserver    'keyserver.ubuntu.com'
+  key          'D294A752'
+end
+
+package 'nginx'
+package 'btsync'
